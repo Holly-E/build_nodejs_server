0db7d0c28adc46f7d82860db1463caf160a49520
Holly Erickson <Erickson.Holly@icloud.com>
2017-12-11 20:33:36 -0600

    node.js serves html doc

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const http = require('http');
+const fs = require('fs');
 
 const host = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+ fs.readFile('./public/index.html', 'utf8', (err, data) => {
+   if (err) {
+     res.writeHead(404);
+     res.end('404 error, file not found');
+   } else {
+     res.writeHead(200, {
+       'Content_Type': 'text/html'
+     });
+     res.end(data);
+   }
+ })
 });
 
 server.listen(port, host, () => {
