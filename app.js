267f6ed715b8e40bdc5f9bc86c651761606be83f
Holly Erickson <Erickson.Holly@icloud.com>
2017-12-11 21:49:06 -0600

    Display request and response data

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 const host = 'localhost';
 const port = 3000;
 
+let reqProps = ['url', 'method', 'httpVersion', 'headers'];
+let resProps = ['statusMessage', 'statusCode', '_header'];
+
 const server = http.createServer((req, res) => {
  fs.readFile('./public/index.html', 'utf8', (err, data) => {
    if (err) {
@@ -13,9 +16,18 @@ const server = http.createServer((req, res) => {
      res.writeHead(200, {
        'Content_Type': 'text/html'
      });
+
+     let request = reqProps.map(function(props) {
+       return { [props] : req[props] };
+     });
+     let response = resProps.map(function(props) {
+       return { [props] : res[props] };
+     });
+     data = data.replace("{{ req }}", JSON.stringify(request, null, 2))
+                .replace("{{ res }}", JSON.stringify(response, null, 2));
      res.end(data);
    }
- })
+ });
 });
 
 server.listen(port, host, () => {
